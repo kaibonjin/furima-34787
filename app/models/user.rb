04bb018383211835b683f4dcc51ff93af2c3c819
kaibonjin <kaibonjin@gmail.com>
2021-04-01 16:35:03 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
-  validates :last_name, presence: true, #/^[ぁ-んァ-ン一-龥]/
-  validates :first_name, presence: true , #/^[ぁ-んァ-ン一-龥]/
-  validates :last_name_kana, presence: true, #/^([ァ-ン]|ー)+$/
-  validates :first_name_kana, presence: true, #/^([ァ-ン]|ー)+$/
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birthday, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} , message: 'には英字と数字の両方を含めて設定してください' 
 end
