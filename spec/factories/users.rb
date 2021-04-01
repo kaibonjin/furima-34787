dea196c6be92c206c2a788ddb94c7129320738af
kaibonjin <kaibonjin@gmail.com>
2021-04-01 20:22:01 +0900

    テストコード記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.initials(number: 2)}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name {'安田'}
+    first_name {'太郎'}
+    last_name_kana {'ヤスダ'}
+    first_name_kana {'タロウ'}
+    birthday {'1997-03-03'}
+  end
+end
