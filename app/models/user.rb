576b0d67548135fbfe0b5eeb752aa70b1956b0aa
kaibonjin <kaibonjin@gmail.com>
2021-04-01 16:11:24 +0900

    バリデーションのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, #/^[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true , #/^[ぁ-んァ-ン一-龥]/
+  validates :last_name_kana, presence: true, #/^([ァ-ン]|ー)+$/
+  validates :first_name_kana, presence: true, #/^([ァ-ン]|ー)+$/
   validates :birthday, presence: true
 end
