32a13f1e2c829ad0dfc1c4f6a7013188bee17700
kaibonjin <kaibonjin@gmail.com>
2021-04-02 18:08:53 +0900

    activehashモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  def new
+    @item = Item.new
+  end
+  def create 
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :days_till_ship
+  
   belongs_to :user
   has_one_attached :image
-  validates 
+  
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :condition_id, presence: true, numericality: { other_than: 1 } 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 } 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 } 
+  validates :days_till_ship_id, presence: true, numericality: { other_than: 1 } 
+  validates :price, presence: true
 end
