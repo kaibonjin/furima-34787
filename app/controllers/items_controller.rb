24f50917144749956014feff9286f74c11662943
kaibonjin <kaibonjin@gmail.com>
2021-04-04 22:33:57 +0900

    販売手数料と販売利益javascript実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,10 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index]
   def index
     @items = Item.all
   end
   def new
     @item = Item.new
-
   end
   def create
     @item = Item.new(items_params)
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,15 @@ class Item < ApplicationRecord
   
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 } 
-  validates :condition_id, presence: true, numericality: { other_than: 1 } 
-  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 } 
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 } 
-  validates :days_till_ship_id, presence: true, numericality: { other_than: 1 } 
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :days_till_ship_id, presence: true, numericality: { other_than: 1 }
   validates_inclusion_of :price, in: 300..9999999, message: "must be between 300~9999999"
   validates :price, format: {with: /\A[0-9]+\z/}
   validates :price, presence: true
   validates :image, presence: true
 end
+
+#エラーメッセージをちゃんといれたい
