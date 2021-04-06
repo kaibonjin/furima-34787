3f299f3092029dff3b2f2ab35102fc45658b3db6
kaibonjin <kaibonjin@gmail.com>
2021-04-06 18:41:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,8 @@ class ItemsController < ApplicationController
     redirect_to action: :index unless current_user.id == @item.user_id
     if @item.destroy
       redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
