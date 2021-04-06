74fcc54d5b4a06b9752941e1e77a650e846c9299
kaibonjin <kaibonjin@gmail.com>
2021-04-06 18:01:50 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,15 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    redirect_to action: :index unless current_user.id == @item.user_id
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def items_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_till_ship_id, :price, :image).merge(user_id: current_user.id)
