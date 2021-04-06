c3bc432a355757e85ffa2036b5fc285d5863e76c
kaibonjin <kaibonjin@gmail.com>
2021-04-06 18:25:00 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :findparams, only: [:show, :destroy]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -16,11 +18,9 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user_id
     if @item.destroy
       redirect_to root_path
@@ -30,5 +30,8 @@ class ItemsController < ApplicationController
   private
   def items_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_till_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def findparams
+    @item = Item.find(params[:id])
   end
 end
